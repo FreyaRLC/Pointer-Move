b8d6cf489479415e8086ad2d2f96995caf7bf0a6
Freya Luntang Christensen <frey3045@stud.kea.dk>
2023-09-07 09:46:52 +0200

    hue changes on mouse y-coordinate

--- js.js
+++ js.js
@@ -1,19 +1,22 @@
 "use strict";
-let lightness = 40;
+let lightness;
+let hue;
 
 document.addEventListener("mousemove", changeBackground);
 
 function changeBackground(e) {
   lightness = Math.round((e.clientX / window.innerWidth) * 100);
-  let hsl = `hsl(290, 40%, ${lightness}%)`;
-  document.querySelector(
-    "#lightness"
-  ).textContent = `background color is: ${hsl}`;
+  hue = Math.round((e.clientY / window.innerHeight) * 360);
+
+  let hsl = `hsl(${hue}, 50%, ${lightness}%)`;
+
+  document.querySelector("#hsl").textContent = `background color is: ${hsl}`;
+
   document.querySelector("body").style.backgroundColor = hsl;
 
   if (lightness < 50) {
-    document.querySelector("#lightness").style.color = "white";
+    document.querySelector("#hsl").style.color = "white";
   } else {
-    document.querySelector("#lightness").style.color = "black";
+    document.querySelector("#hsl").style.color = "black";
   }
 }
